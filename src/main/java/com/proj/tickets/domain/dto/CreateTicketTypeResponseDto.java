f5ccf997f0495637f8502750207d391051edac08
rachit-890 <rachitkushwaha890@gmail.com>
2025-08-15 08:53:04 +0530

    added request and response dto

--- src/main/java/com/proj/tickets/domain/dto/CreateTicketTypeResponseDto.java
+++ src/main/java/com/proj/tickets/domain/dto/CreateTicketTypeResponseDto.java
@@ -5,23 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
-import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateEventResponseDto {
+public class CreateTicketTypeResponseDto {
+
     private UUID id;
     private String name;
-    private LocalDateTime start;
-    private LocalDateTime end;
-    private String venue;
-    private LocalDateTime saleStart;
-    private LocalDateTime saleEnd;
-    //    private String status;
-    private List<CreateTicketTypeResponseDto> ticketTypes;
+    private String description;
+    private Double price;
+    private Integer totalAvailable;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
 }
